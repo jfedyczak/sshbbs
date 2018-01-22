fec91127b691c6bfe43b85b9bf435c656971b3ac
Jakub Fedyczak <jakub@fedyczak.net>
2018-01-22 16:59:07 +0100

    Blessed

--- go.js
+++ go.js
@@ -5,10 +5,36 @@ const
 	crypto = require('crypto'),
 	inspect = require('util').inspect,
 	ssh2 = require('ssh2'),
-	utils = ssh2.utils
+	utils = ssh2.utils,
+	blessed = require('blessed')
 
 let pubKey = utils.genPublicKey(utils.parseKey(fs.readFileSync('authorized_keys')))
+
+const processConnection = screen => {
+	screen.data.main = blessed.box({
+		parent: screen,
+		width: '80%',
+		height: '90%',
+		border: 'line',
+		content: 'Hello!',
+		style: {
+			fg: 'white',
+			bg: 'red',
+		}
+	})
 	
+	screen.key('i', () => {
+		screen.data.main.style.bg = 'blue'
+		screen.render()
+	})
+	
+	screen.key(['C-c', 'q'], (ch, key) => {
+		screen.destroy()
+	})
+	
+	screen.render()
+}
+
 new ssh2.Server({
 	hostKeys: [fs.readFileSync('host.key')]
 }, client => {
@@ -44,29 +70,50 @@ new ssh2.Server({
 	client.on('ready', () => {
 		console.log(' -- client authenticated')
 		client.on('session', (accept, reject) => {
+			let screen
+			let term
 			let session = accept()
-			session.once('shell', (accept, reject) => {
+			session.on('shell', (accept, reject) => {
 				console.log(` -- shell request`)
 				let stream = accept()
-				stream.on('data', (data) => {
-					console.log(data.toString('utf8'))
+				screen = blessed.screen({
+					smartCSR: true,
+					terminal: 'xterm-256color',
+					fullUnicode: true,
+					input: stream,
+					output: stream,
 				})
-				stream.write(` -- hello, it is ${new Date()}\r\n\r\n`)
-				setTimeout(() => {
+				screen.on('destroy', () => {
 					stream.exit(0)
 					stream.end()
-				}, 5000)
+				})
+				processConnection(screen)
 			})
 			session.on('pty', (accept, reject, info) => {
 				console.log(` -- pty requested: ${JSON.stringify(info)}`)
+				term = info.term
 				accept()
 			})
+			session.on('window-change', (accept, reject, info) => {
+				if (accept) accept()
+				console.log(`${info.cols} x ${info.rows}`)
+				if (screen) {
+					screen.cols = info.cols
+					screen.rows = info.rows
+				}
+				session.emit('resize')
+			})
 		})
-		client.on('request', (accept, reject, name, info) => {
-			console.log(` -- request: ${name}`)
-			console.log(info)
-			reject()
+		client.on('close', () => {
+			if (screen && !screen.destroyed) {
+				screen.destroy()
+			}
 		})
+		// client.on('request', (accept, reject, name, info) => {
+		// 	console.log(` -- request: ${name}`)
+		// 	console.log(info)
+		// 	reject()
+		// })
 	})
 }).listen(50505, '127.0.0.1', function() {
 	console.log(` -- server ready at port ${this.address().port}`)
